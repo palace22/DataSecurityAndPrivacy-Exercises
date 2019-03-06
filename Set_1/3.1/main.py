7488b68d9e2f48e97c5da6f85d6983c661c59458
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-03-06 23:24:51 +0100

    fix: text lenght

--- Set_1/3.1/main.py
+++ Set_1/3.1/main.py
@@ -9,9 +9,11 @@ def histogram( char_array:list ) -> dict:
     letters_dict = { x : char_array.count(x) for x in char_array }
     return letters_dict
     
-def empiric_distribution( char_array:list, n_gram:int ) -> dict:
-    n_gram_array = [ ''.join(x) for x in char_array[x:x+n_gram] for x in range(0, len(char_array), n_gram)]   
-    n_gram_distribution = { x : n_gram_array.count(x)/len(x) for x in n_gram_array} 
+def empirical_distribution( char_array:list, n_gram:int ) -> dict:
+    alphabet_size = 26**n_gram
+    n_gram_array = [char_array[x:x+n_gram] for x in range(0, len(char_array), n_gram)]   #create a structure of arrays of n_gram elements
+    n_gram_array = [ ''.join(x) for x in n_gram_array] #transform structure of arrays in an array of n_gram size string element
+    n_gram_distribution = { x : n_gram_array.count(x)/alphabet_size for x in n_gram_array} #calculate occurrences distribution
     return n_gram_distribution
 
 def coincidence_index( hist: dict, text_len:int)->float:
