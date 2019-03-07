8bf5fc80382db69da04c89ec128cae313abcc06f
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-03-07 21:01:00 +0100

    feat: extend histogram to n-gram

--- Set_1/3.1/main.py
+++ Set_1/3.1/main.py
@@ -1,16 +1,21 @@
 import string
 import os
+import matplotlib.pyplot as plot
 
 def toArray26( text: str):
     array_char = list(text.replace(' ','').lower())    
     array_char = [ord(c)-97 for c in array_char]
 
     return array_char
-
-def histogram( char_array:list ) -> dict:
-    letters_dict = { x : char_array.count(x) for x in char_array }
-    return letters_dict
     
+def n_gram_histogram( char_array:list, n_gram:int = 3, occurrence: int = 3, plot_hist: bool  = False ) -> dict:
+    n_gram_array = [ ''.join(char_array[x:x+n_gram]) for x in range(0, len(char_array), n_gram)]   #create a an array of n_gram size element
+    n_gram_distribution = { x : n_gram_array.count(x) for x in n_gram_array if n_gram_array.count(x) > occurrence} #calculate occurrences and select them accorder to occurrence argument
+    if plot_hist:
+        plot.bar(n_gram_distribution.keys(), n_gram_distribution.values())
+        plot.show()
+    return n_gram_distribution
+
 def empirical_distribution( char_array:list, n_gram:int ) -> dict:
     alphabet_size = 26**n_gram
     n_gram_array = [char_array[x:x+n_gram] for x in range(0, len(char_array), n_gram)]   #create a structure of arrays of n_gram elements
