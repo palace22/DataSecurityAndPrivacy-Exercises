3b261e2cd35527956f874f3d0944262dcf4d0e15
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-03-23 18:35:37 +0100

    feat: add entropy function

--- Set_1/3.1/main.py
+++ Set_1/3.1/main.py
@@ -1,50 +1,60 @@
 import string
 import os
+from math import log
 import matplotlib.pyplot as plot
+from english_distribution import en_frequency
 
-def toArray26( text: str):
-    array_char = list(text.replace(' ','').lower())    
-    array_char = [ord(c)-97 for c in array_char]
 
-    return array_char
+def n_gram_histogram(char_array: list, n_gram: int = 3, occurrence: int = 3, plot_hist: bool = False) -> dict:
+    n_gram_array = ["".join(char_array[x : x + n_gram]) for x in range(0, len(char_array), n_gram)]  # create a an array of n_gram size element
+    n_gram_distribution = {x: n_gram_array.count(x) for x in n_gram_array if n_gram_array.count(x) > occurrence }  # calculate occurrences and select them accorder to occurrence argument
     
-def n_gram_histogram( char_array:list, n_gram:int = 3, occurrence: int = 3, plot_hist: bool  = False ) -> dict:
-    n_gram_array = [ ''.join(char_array[x:x+n_gram]) for x in range(0, len(char_array), n_gram)]   #create a an array of n_gram size element
-    n_gram_distribution = { x : n_gram_array.count(x) for x in n_gram_array if n_gram_array.count(x) > occurrence} #calculate occurrences and select them accorder to occurrence argument
     if plot_hist:
         plot.bar(n_gram_distribution.keys(), n_gram_distribution.values())
-        [plot.text(a, b, b, horizontalalignment='center') for a,b in n_gram_distribution.items()]
+        [plot.text(a, b, b, horizontalalignment="center") for a, b in n_gram_distribution.items()]
         plot.show()
     return n_gram_distribution
 
-def empirical_distribution( char_array:list, n_gram:int ) -> dict:
-    alphabet_size = 26**n_gram
-    n_gram_array = [char_array[x:x+n_gram] for x in range(0, len(char_array), n_gram)]   #create a structure of arrays of n_gram elements
-    n_gram_array = [ ''.join(x) for x in n_gram_array] #transform structure of arrays in an array of n_gram size string element
-    n_gram_distribution = { x : n_gram_array.count(x)/alphabet_size for x in n_gram_array} #calculate occurrences distribution
+
+def empirical_distribution(char_array: list, n_gram: int) -> dict:
+    alphabet_size = 26 ** n_gram
+    n_gram_array = [char_array[x : x + n_gram] for x in range(0, len(char_array), n_gram)]  # create a structure of arrays of n_gram elements
+    n_gram_array = ["".join(x) for x in n_gram_array]  # transform structure of arrays in an array of n_gram size string element
+    n_gram_distribution = {x: n_gram_array.count(x) / alphabet_size for x in n_gram_array}  # calculate occurrences distribution
     return n_gram_distribution
 
-def n_gram_frequency( char_array:list, n_gram:int ) -> dict:
-    alphabet_size = len(char_array)/n_gram
-    n_gram_array = [char_array[x:x+n_gram] for x in range(0, len(char_array), n_gram)]   #create a structure of arrays of n_gram elements
-    n_gram_array = [ ''.join(x) for x in n_gram_array] #transform structure of arrays in an array of n_gram size string element
-    n_gram_distribution = { x : n_gram_array.count(x)/alphabet_size for x in n_gram_array} #calculate occurrences distribution
+
+def text_entropy(char_array: list, text_len: int) -> int:
+    char_array_prob, entropy = {k: v / text_len for k, v in en_frequency.items()}, 0 #from frequencies to probrabilities
+    for c in char_array: entropy += char_array_prob[c]*log(char_array_prob[c], 2) #calculate entropy
+    return entropy
+
+
+def n_gram_frequency(char_array: list, n_gram: int) -> dict:
+    alphabet_size = len(char_array) / n_gram
+    n_gram_array = [char_array[x : x + n_gram] for x in range(0, len(char_array), n_gram)]  # create a structure of arrays of n_gram elements
+    n_gram_array = ["".join(x) for x in n_gram_array]  # transform structure of arrays in an array of n_gram size string element
+    n_gram_distribution = {x: n_gram_array.count(x) / alphabet_size for x in n_gram_array}  # calculate occurrences distribution
     return n_gram_distribution
 
-def coincidence_index( hist: dict, text_len:int)->float:
-    denom = text_len*(text_len-1)
-    c_index = [ ((x*(x-1))/denom) for _,x in hist.items() ]
+
+def coincidence_index(hist: dict, text_len: int) -> float:
+    denom = text_len * (text_len - 1)
+    c_index = [((x * (x - 1)) / denom) for _, x in hist.items()]
     return sum(c_index)
 
-def run():
-    with open(os.path.dirname(__file__) + ".."+os.sep+".."+os.sep+"MobiDickCap1.txt", 'r') as myfile:
-        text=myfile.read().replace('\n', '')
-    
-    char_array = list(filter(str.isalpha, text.replace(' ','').lower()))
-    char_array = [s.translate(str.maketrans('', '', string.punctuation)) for s in char_array]   
 
-    c_index = coincidence_index( n_gram_histogram(char_array, 1, 0), len(char_array))
-    n_gram_hist = n_gram_histogram( char_array, 3, 7, True )
+def run():
+    with open(os.path.dirname(__file__) + ".." + os.sep + ".." + os.sep + "MobiDickCap1.txt","r") as myfile:
+        text = myfile.read().replace("\n", "")
+
+    char_array = list(filter(str.isalpha, text.replace(" ", "").lower()))
+    char_array = [s.translate(str.maketrans("", "", string.punctuation)) for s in char_array]
+    
+    text_entropy( char_array, len(char_array))
+    c_index = coincidence_index(n_gram_histogram(char_array, 1, 0), len(char_array))
+    n_gram_hist = n_gram_histogram(char_array, 3, 7, True)
+
 
 if __name__ == "__main__":
     run()
